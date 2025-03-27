eda8cffd8d5b2789571c9cdafc9cf7dbeb3714d1
Masy Jordan <masyjordan@gmail.com>
2025-03-27 11:01:14 +0100

    new structure

---
+++ src/backend/api/fetchWeatherApi.js
@@ -0,0 +1,8 @@
+export default function fetchWeatherApi(city, location) {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
--- vite.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/Wozniak-Weather/', // Remplace par le nom exact de ton repo GitHub
-});
